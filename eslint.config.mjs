0c417f029406f8db09c5793addddbf8900d4c2e5
beefchimi <curtdulmage@gmail.com>
2024-05-24 16:24:24 -0400

    :hammer_and_wrench: [Eslint] Add no-console rule to config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,21 @@ import configLove from 'eslint-config-love'
 export default [
   configLove,
   {
-    ignores: ['coverage/*', 'dist/*']
+    rules: {
+      'no-console': 'error'
+    }
   }
 ]
+
+/*
+  {
+    languageOptions: {
+      sourceType: "module",
+    },
+    ignores: ["coverage/*", "dist/*"],
+    files: ["*.js", "*.ts"],
+    rules: {
+      "no-console": "error",
+    },
+  },
+*/
