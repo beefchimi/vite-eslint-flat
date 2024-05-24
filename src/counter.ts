e2202d4351913fb118662edffcbe730326c6e290
beefchimi <curtdulmage@gmail.com>
2024-05-24 16:24:54 -0400

    :alembic: [Counter] Add a console.log

--- src/counter.ts
+++ src/counter.ts
@@ -1,9 +1,17 @@
+/* eslint no-console: "error" */
+
 export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
+  let counter = 0;
+
   const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+
+    // This should cause an eslint error!
+    console.log("New count:", counter);
+  };
+
+  element.addEventListener("click", () => setCounter(counter + 1));
+
+  setCounter(0);
 }
