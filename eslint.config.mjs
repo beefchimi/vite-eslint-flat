eafa0b2d2536819a1f8607c63e4d5fc41298a449
beefchimi <curtdulmage@gmail.com>
2024-05-30 11:57:01 -0400

    :hammer_and_wrench: [Eslint] Move ignores to be the first config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,28 @@
+import globals from 'globals';
 import configLove from 'eslint-config-love';
 // Includes both `config` and `plugin`.
 import pluginPrettier from 'eslint-plugin-prettier/recommended';
 
 export default [
+  {
+    ignores: ['coverage/**', 'dist/**'],
+  },
   configLove,
   pluginPrettier,
   {
+    name: 'custom-rules',
     files: ['**/*.ts', '**/*.js', '**/*.mjs'],
-    ignores: ['coverage/**', 'dist/**'],
-    rules: {
-      'no-console': 'warn',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-    },
-  },
-
-  /*
-  import globals from 'globals';
-
-  {
     languageOptions: {
       ...configLove.languageOptions,
-      parserOptions: {
-        ...configLove.languageOptions.parserOptions,
-        project: './tsconfig.json',
-      },
       globals: {
         ...globals.browser,
       },
       ecmaVersion: 2022,
       sourceType: 'module',
     },
-  */
+    rules: {
+      'no-console': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+  },
 ];
